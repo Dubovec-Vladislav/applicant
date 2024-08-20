85c54f3a2c3b670aae673bee842b878bd056a604
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2024-08-20 15:00:41 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { DatePicker, Steps } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Steps
+        current={1}
+        items={[
+          {
+            title: "Finished",
+            description: "This is a description.",
+          },
+          {
+            title: "In Progress",
+            description: "This is a description.",
+            subTitle: "Left 00:00:08",
+          },
+          {
+            title: "Waiting",
+            description: "This is a description.",
+          },
+        ]}
+      />
     </div>
   );
 }
